c571f13cf428dbaf18a44c059cbbdc3883025a73
annet37 <83719069+annet37@users.noreply.github.com>
2021-10-30 18:41:47 +0300

    bootom

--- app/src/main/java/com/example/homework20/MainActivity.kt
+++ app/src/main/java/com/example/homework20/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.homework20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onClickToast(view:View){
+        Toast.makeText(this,"oK", Toast.LENGTH_SHORT).show()
     }
 }
